843fbb43e0a2f7b8ed807db89e35e1aeede1e87b
Joshua Favo <jfavo2@uis.edu>
2019-12-20 11:36:06 -0500

    12/20/2019

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
+    browsers: ['Firefox', 'FirefoxDeveloper', 'FirefoxAurora', 'FirefoxNightly'],
     singleRun: false,
     restartOnFileChange: true
   });
